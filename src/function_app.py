c3d70c3deee7bb3f54e797633ec1acdeb9cd0575
LORENZO PONCIO <lorenzo.poncio@univille.br>
2026-05-07 21:48:27 -0300

    Feat: src: criação de azure function

--- src/function_app.py
+++ src/function_app.py
@@ -9,4 +9,22 @@ def timer_trigger_aula2(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('Python timer trigger function executed.')
+    logging.info('Inter')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger_aula3(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Inter colossal')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger_aula4(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Inter gigantesco')
