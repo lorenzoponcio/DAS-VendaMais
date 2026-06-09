325d3c2e34fdc80a2ee985a40eb07d0bc4fd2b2f
lorenzoponcio <lorenzoponcio99@gmail.com>
2026-06-09 14:30:12 -0300

    Fix: Ajuste na biblioteca psycopg2 para conexão correta

--- src/triggers/poc_psycopg2.py
+++ src/triggers/poc_psycopg2.py
@@ -5,8 +5,12 @@ import psycopg2
 
 bp = func.Blueprint()
 
-@bp.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
-                   use_monitor=False)
+@bp.timer_trigger(
+    schedule="0 * * * * *",
+    arg_name="myTimer",
+    run_on_startup=False,
+    use_monitor=False
+)
 def poc_psycopg2(myTimer: func.TimerRequest) -> None:
 
     sql_server = os.getenv('SQL_SERVER_SOURCE')
@@ -14,34 +18,27 @@ def poc_psycopg2(myTimer: func.TimerRequest) -> None:
     sql_user = os.getenv('SQL_USER_SOURCE')
     sql_pass = os.getenv('SQL_PASSWORD_SOURCE')
 
+    logging.info(f"servidor: {sql_server}, banco: {sql_database}, usuario: {sql_user}")
 
-    logging.info(f"""servidor: {sql_server}, banco: {sql_database}, usuario: {sql_user}, senha: {sql_pass}""")
-
-    conn_str = psycopg2.connect(
-        
-        database=sql_database,
-        user=sql_user,
-        password=sql_pass,
-        host=sql_server
-        # port="5432"
-    )
-
-   
     try:
-        with psycopg2.connect(conn_str) as conn:
-            # Cria um cursor para executar a consulta   
-            cursor = conn.cursor()
-            
-            query = "select top 5 * from erp.pedido_item"
+        with psycopg2.connect(
+            database=sql_database,
+            user=sql_user,
+            password=sql_pass,
+            host=sql_server,
+            port=5432
+        ) as conn:
 
-            # Executa a consulta SQL
-            cursor.execute(query)
+            with conn.cursor() as cursor:
+                query = "SELECT * FROM erp.pedido_item LIMIT 5"
 
-            # Busca todos os resultados da consulta
-            rows = cursor.fetchone()
+                cursor.execute(query)
 
-            logging.info(rows)           
+                rows = cursor.fetchall()
+
+                for row in rows:
+                    logging.info(row)
 
     except Exception as e:
-        logging.error(f"Erro ao ler erp.pedido: {str(e)}")
+        logging.error(f"Erro ao ler erp.pedido_item: {str(e)}")
         raise
